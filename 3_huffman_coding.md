b9b8a7ab3cfa8ca3b2555f369da5ef69c8aafb66
valmsmith39a <valmsmith39a@gmail.com>
2020-05-03 16:34:44 -0700

    huffman coding: add explanation

---
+++ 3_huffman_coding.md
@@ -0,0 +1,38 @@
+## Explanation
+
+### Discussion
+To build the frequency map to track the frequency for each character,
+I used a dictionary for fast insertion/lookup.
+
+As a foundation for the Huffman Tree, I used a min heap which is a 
+binary tree in which each node is less than or equal to the child nodes.
+
+To build the min-heap I started by building a priority queue with nodes
+storing the character and frequency.
+
+### Time complexity
+
+* Get the frequencies for each character by looping through each character, which takes O(n) time.
+
+* We build the priority queue by looping through frequency list, which takes O(n) time.
+
+* Building the min-heap using heapify takes O(n) time.
+  https://www.growingwiththeweb.com/data-structures/binary-heap/build-heap-proof/
+
+* To build the Huffman Tree from min-heap, we perform heappop and heappush operations which both take 
+  take O(log(n)) time. We perform the operation for N items, so the result is O(n log n) time
+  https://www.cs.auckland.ac.nz/software/AlgAnim/huffman.html
+
+* Decoding entails looping through the string of bits which takes O(n) time.
+
+### Space complexity
+
+* Creating the dictionary and list of tuples take O(n) space.
+
+* Building the priority queue takes O(n) space.
+
+* Building the min-heap using heapify takes O(n) space.
+
+* The number of nodes used to building the Huffman Tree increases memory space on the order of O(n).
+
+* We decode by constructing a string of characters, which takes O(n) space.
--- 3_huffman_coding.py
+++ 3_huffman_coding.py
@@ -3,6 +3,19 @@ from heapq import heappush, heappop, heapify
 
 
 def huffman_encoding(data):
+    """
+    Data compression by Huffman coding algorithm.
+
+    Parameters:
+        data (str): string data to encode 
+
+    Returns:
+        huffman_code (str): encoded string of bits
+        root_node (class: Node): root node of Huffman Tree
+    """
+
+    if data == "" or data is None:
+        return None, None
 
     # phase 1: build the Huffman Tree
 
@@ -49,6 +62,15 @@ def huffman_encoding(data):
 
 
 def huffman_decoding(data, tree):
+    """
+    Decode Huffman code
+
+    Parameters:
+        data (str): string of bits 
+
+    Returns:
+        decoded (str): decoded data in original form
+    """
     current = tree
     decoded = ""
 
@@ -65,6 +87,15 @@ def huffman_decoding(data, tree):
 
 
 def get_frequencies(data):
+    """
+    Get frequencies for each character.
+
+    Parameters:
+        data (str): data string
+
+    Returns:
+        frequencies (list): list of tuples containing frequency and character
+    """
     freq_dict = dict()
     frequencies = []
 
@@ -83,6 +114,16 @@ def get_frequencies(data):
 
 
 def generate_huffman_codes(node, prefix):
+    """
+    Generate Huffman codes for each character by traversing the tree 
+    and assigning '0' to a move towards left child node and '1' to right child node.
+
+    Parameters:
+        node (class: Node): root node of Huffman Tree
+        prefix (str): starting prefix
+    Returns:
+        huffman_codes (dictionary): map of characters to generated Huffman codes 
+    """
 
     huffman_codes = {}
 
@@ -100,6 +141,16 @@ def generate_huffman_codes(node, prefix):
 
 
 class Node:
+    """
+    Create a node used to build a tree.
+
+    Attributes:
+        left_child (class: Node): left child of a node
+        right_child (class: Node): right child of a node
+        freq: frequency of a character
+        char: character 
+    """
+
     def __init__(self, freq, char=None):
         self.left_child = None
         self.right_child = None
@@ -110,17 +161,18 @@ class Node:
         return self.freq < other.freq
 
 
-if __name__ == "__main__":
-    codes = {}
+def print_test_results(input_data):
 
-    a_great_sentence = "The bird is the word"
-
-    print("The content of the data is: {}\n".format(a_great_sentence))
+    print("The content of the data is: {}\n".format(input_data))
 
     print("The size of the data is: {}\n".format(
-        sys.getsizeof(a_great_sentence)))
+        sys.getsizeof(input_data)))
 
-    encoded_data, tree = huffman_encoding(a_great_sentence)
+    encoded_data, tree = huffman_encoding(input_data)
+
+    if encoded_data is None:
+        print("No input data")
+        return
 
     print("The content of the encoded data is: {}\n".format(encoded_data))
 
@@ -133,6 +185,74 @@ if __name__ == "__main__":
 
     print("The size of the decoded data is: {}\n".format(
         sys.getsizeof(decoded_data)))
+
+
+if __name__ == "__main__":
+
+    input_data = ""
+
+    # *** TEST 1 ***
+
+    print("--- Test 1 ---")
+
+    input_data = "The bird is the word"
+
+    print_test_results(input_data)
+
+    """
+    The content of the data is: The bird is the word
+
+    The size of the data is: 69
+
+    The content of the encoded data is: 0000010111011010100011111011110001100111010110101110110000110001111011
+
+    The size of the encoded data is: 36
+
+    The content of the decoded data is: The bird is the word
+
+    The size of the decoded data is: 69
+    """
+
+    # *** TEST 2 ***
+
+    print("--- Test 2 ---")
+
+    input_data = """His name was Gaal Dornick and he was just a country boy who had never seen Trantor before.
+    That is, not in real life. He had seen it many times on the hyper-video, and occasionally in tremendous
+    three-dimensional newscasts covering an Imperial Coronation or the opening of a Galactic Council."""
+
+    print_test_results(input_data)
+
+    """
+    The content of the data is: His name was Gaal Dornick and he was just a country boy who had never seen Trantor before.
+    That is, not in real life. He had seen it many times on the hyper-video, and occasionally in tremendous
+    three-dimensional newscasts covering an Imperial Coronation or the opening of a Galactic Council.
+
+    The size of the data is: 349
+
+    The content of the encoded data is: 011001010001111100110111001011111110000110001100111110001101001100110010010001011000110101111011011000011110111000100110011010100100100111110000110001100111110010110000011011111110101001100000111110100110111101010111110101101000100010101010110100011000100111010001001111000100100110111101011100111011110001111111101110110100011101111110110011010101101011110000100010111010110011010111101110101110101110100000000001110111001111000101001000111110110011001101101001010010001101001111011101100100100010010100010110011110101110100011001011100010011110001001001111111101110110100100001010010111111001101101101000101100010111111101111100101011010001011001111100010011101101010000111011110011100110111001000010011110101001100110011001101010010010100111101111110011111100010101101110010010100101011010010001101000101111101110101111111011010100110100110111111101110100000000001011001111110111011100111001010011000101111111011011111110001010110111001001000110111100110001111101111110011111010111111000111110101011100111011110100011010110101001100110100011100001011110100001110111101000110010010000100011101011110101011011100010110001010110100101011110000101100111110001010010000111011011000110101101010010101011001001100000110100110010010110001111010110000111100010001110100110111101011111000100101011101
+
+    The size of the encoded data is: 200
+
+    The content of the decoded data is: 
+    His name was Gaal Dornick and he was just a country boy who had never seen Trantor before.
+    That is, not in real life. He had seen it many times on the hyper-video, and occasionally in tremendous
+    three-dimensional newscasts covering an Imperial Coronation or the opening of a Galactic Council.
+
+    The size of the decoded data is: 349
+    """
+
+    # *** TEST 3 ***
+
+    print("--- Test 3 ---")
+
+    input_data = ""
+
+    print_test_results(input_data)
+
+    """
+    No input data
+    """
 
 # Resources:
 # https://www.studytonight.com/data-structures/huffman-coding
